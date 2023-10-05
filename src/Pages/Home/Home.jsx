007ad4e217f797c4cc7e55cda32aa733b64c6589
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-10-05 22:24:55 +0600

    Navbar Added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>This is home</h1>
+        </div>
+    );
+};
+
+export default Home;
